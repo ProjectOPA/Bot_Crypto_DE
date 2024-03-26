5df88fc02cc75eb542808027adba4c5db39cd603
Khaty GUYET <k.guyet@gmail.com>
2024-03-26 10:56:23 +0100

    Update producer.py

--- Application/producer.py
+++ Application/producer.py
@@ -7,37 +7,36 @@ from binance import BinanceSocketManager
 from binance.enums import *
 import json
 
-kafka_conf = {'bootstrap.servers': 'localhost:9092'}
+kafka_conf = {"bootstrap.servers": "localhost:9092"}
 
-#Création de notre producer
+# Création de notre producer
 producer = Producer(kafka_conf)
 
-async def main():
-    #api_key = 'mVJMDUgghiBBlgQTW73iB5PQJMtU32qny0eFWRnEAfh5VSsa4lgDXWSZhujuQXZ6'
-    #api_secret = 'rHMU91gAKVQvD34HMKFm1HxKUbWjnhwWI7qKtPTqH5JmkgcrNaNgyVUVt1N9HLok'
-    api_secret = '<api_secret>'
-    api_key = '<api_key>'
 
+async def main():
+    api_secret = "<api_secret>"
+    api_key = "<api_key>"
 
     client = await AsyncClient.create()
     bm = BinanceSocketManager(client)
-    
+
     # Création du socket d'écoute sur les klines du BTCUSDT
-    ks = bm.kline_socket('BTCUSDT', interval=KLINE_INTERVAL_5MINUTE)
-    
+    ks = bm.kline_socket("BTCUSDT", interval=KLINE_INTERVAL_5MINUTE)
+
     # Réception permanente du message
     async with ks as tscm:
         while True:
             res = await tscm.recv()
-            
+
             # S'assure que la donnée reçu correspond bien à celle de la fermeture de la k_line afin de respecter les 5MIN d'interval
-            if res['k']['x']:
+            if res["k"]["x"]:
                 # Envoi des données au topic Kafka
-                producer.produce('BTCUSDT_topic', json.dumps(res))
+                producer.produce("BTCUSDT_topic", json.dumps(res))
                 producer.poll(0)  # Appel poll pour s'assurer que le message est envoyé
-                
+
                 # Affichage des données pour vérification
                 print(res)
+
 
 if __name__ == "__main__":
     try:
