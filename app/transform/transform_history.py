5596f8e20c458d88d3d95fb2a4eea95eb28c006c
Khaty GUYET <k.guyet@gmail.com>
2024-03-30 10:24:36 +0100

    Update transform_history.py

--- app/transform/transform_history.py
+++ app/transform/transform_history.py
@@ -1,6 +1,7 @@
 # 2. step2_transform : Transformation (pré-processing)
-# - Une fois les données extraites, cette étape consiste à transformer les données extraites (gestion des valeurs manquantes, création de nouvelles colonnes, normalisation..)pour l'analyse et le traitement par le modèle de Machine Learning.
-#     - transformation des données historiques stockées
+# - Une fois les données extraites, cette étape consiste à transformer les données extraites
+#   (gestion des valeurs manquantes, création de nouvelles colonnes, normalisation..)
+#   pour l'analyse et le traitement par le modèle de Machine Learning.
 #     - transformation des données de streamings
 import pandas as pd
 import warnings
