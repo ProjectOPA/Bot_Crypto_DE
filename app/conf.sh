72c8411bcb77b0d825d7882a217d3cb86406c3d1
Elvis AKOTEGNON <elvisakotegnon@MBP-de-Elvis.lan>
2024-03-31 04:28:43 +0200

    Updating producer_streaming.py to import model function

--- app/conf.sh
+++ app/conf.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # export PUBLIC_IP=$(curl -s http://ifconfig.me/ip)
-# docker-compose up -d
+#export PUBLIC_IP=$(ipconfig getifaddr en0)
+#docker-compose up --scale kafka=2 -d
+#pid=$!
+#wait $pid
 
-# pid=$!
-# wait $pid
-# docker-compose up --scale kafka=2 -d
-# pid=$!
-# wait $pid
 python3 ./extract/extract_history.py
