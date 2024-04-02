686971da66e7ebcf4fc2582b72878965cbef95d4
Khaty GUYET <k.guyet@gmail.com>
2024-04-02 17:31:35 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+from mongo_query import HistoricalRequest, HistoricalResponse, PredictRequest, PredictResponse
 
-app = FastAPI()
-
+# définition de l'application FastAPI
+# en utilisant la classe FastAPI de FastAPI
+# pour créer une application.
+app = FastAPI(
+    title="API Questions",
+    description=""" 
+                          
+    """,
+    version="1.0.1",
+    openapi_tags=[
+        {"name": "home", "description": "page d'accueil de l'API"},
+        {"name": "historical", "description": "page requete sur les données historiques"},
+        {"name": "predict", "description": "page requete sur les prédictions"},
+    ],
+)
 
 @app.get("/")
 def get_home():
     return {"msg": "Hello World"}
+
+@app.get("/historical"):
+    pass
+    """
+    
+    """
 
 
 @app.get("/predict")
