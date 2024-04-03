d799e425c06d4ab0a287e0367a7c1d9a63dd683a
Khaty GUYET <k.guyet@gmail.com>
2024-04-03 11:38:01 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-
 from .main import app
 
 client = TestClient(app)
@@ -8,4 +7,18 @@ client = TestClient(app)
 def test_get_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {
+        "message": "Bienvenue sur l'API de récupération de données historiques et de prédictions"
+    }
+
+
+def test_get_historical():
+    response = client.get("/historical")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+def test_get_prediction():
+    response = client.get("/predict")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
