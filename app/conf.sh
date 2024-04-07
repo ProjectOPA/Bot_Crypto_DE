0cb133dfc8dd2a75dc083ff8c7f0236a1c595941
Elvis AKOTEGNON <elvisakotegnon@MBP-de-Elvis.lan>
2024-04-07 23:48:46 +0200

    Version 2: Stable sur Windows, Linux et MacOS de OPA App

--- app/conf.sh
+++ app/conf.sh
@@ -2,23 +2,19 @@
 
 # Check the operating system
 if [[ $(uname) == "Linux" ]]; then
-    # If on Linux, set PUBLIC_IP using curl
     #export PUBLIC_IP=$(curl -s http://ifconfig.me/ip)
     echo "Linux Distribution of OPA service"
     export PUBLIC_IP=$(hostname -I | awk '{print $1}')
     echo $PUBLIC_IP
 elif [[ $(uname) == "Darwin" ]]; then
-    # If on macOS, set PUBLIC_IP using ipconfig
     echo "Mac Distribution of OPA service"
     export PUBLIC_IP=$(ipconfig getifaddr en0)
     echo $PUBLIC_IP
 elif [[ $(uname) == "MINGW"* ]]; then
-    # If on Windows (using Git Bash or similar), set PUBLIC_IP using PowerShell
     echo "Windows Distribution of OPA service"
     export PUBLIC_IP=$(powershell.exe -Command "(Test-Connection -ComputerName (hostname) -Count 1).IPAddressToString")
     echo $PUBLIC_IP
 else
-    # Handle other operating systems here if needed
     echo "Unsupported operating system, kakfa service might no be running properly"
     exit 1
 fi
@@ -30,4 +26,4 @@ docker image build Extract_Transform_Hist -t historic_handler:latest
 docker image build  Load_Stream -t stream_loader:latest
 
 # Start Docker containers using docker-compose
-docker-compose up --scale kafka=2 -d
+docker-compose up -d
